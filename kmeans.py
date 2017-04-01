034af9ee2005aafa786ac290b545a4ee9242e614
Antoine Broyelle <antoine.broyelle@protonmail.com>
2017-04-01 14:13:18 +0200

    Refactoring : utils as a package

--- kmeans.py
+++ kmeans.py
@@ -1,14 +1,7 @@
 # Author : Antoine Broyelle
 # Licence : MIT
 import numpy as np
-
-def euclidianDist(data, centers):
-    '''Compute the euclidian distance of each data point to each center'''
-    return np.sqrt(np.sum((data - centers[:, np.newaxis]) ** 2, axis=2))
-
-def manhattanDist(data, centers):
-    '''Compute the Manhattan distance of each data point to each center'''
-    return np.sum(np.abs(data - centers[:, np.newaxis]), axis=2)
+from utils.distances import euclidianDist
 
 class Kmeans:
 
---
+++ utils/distances.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def euclidianDist(data, centers):
+    '''Compute the euclidian distance of each data point to each center'''
+    return np.sqrt(np.sum((data - centers[:, np.newaxis]) ** 2, axis=2))
+
+def manhattanDist(data, centers):
+    '''Compute the Manhattan distance of each data point to each center'''
+    return np.sum(np.abs(data - centers[:, np.newaxis]), axis=2)
+
